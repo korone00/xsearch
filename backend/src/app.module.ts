01b58e0a007d8d673ad5518cec2ccc115bc51cf2
HugeCookie <phrpp5770@gmail.com>
2023-07-19 10:40:26 +0900

    log

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,15 +13,9 @@ import { UserModule } from './users/users.modules';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-<<<<<<< HEAD
       password: '1234',
       database: 'postgres',
       entities: [User], 
-=======
-      password: '0927',
-      database: 'xsearch',
-      entities: [__dirname+'/**/*.entity.{ts,js}'], 
->>>>>>> 2e4f254ab883e7e5cda6614aacb61e53a25be9b5
       synchronize: true,  //shouldn't be used in production - otherwise you can lose production data.
     }),
     UserModule,
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Controller,UseGuards,Post,Req, Get } from "@nestjs/common";
-=======
-import { Controller,UseGuards,Post,Req,Body } from "@nestjs/common";
->>>>>>> 2e4f254ab883e7e5cda6614aacb61e53a25be9b5
 import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./local-auth.guard";
 import { User } from "src/users/entities/user.entity";
@@ -15,7 +11,6 @@ export class AuthController{
     async login(@Req() req){
         return this.authService.login(req.user);
     }// 결과로 할당된 토큰 반환
-<<<<<<< HEAD
     @Get('logout')
     async logout(@Req() req) {
         await this.authService.logout(req.user);
@@ -24,11 +19,3 @@ export class AuthController{
 
 }
 
-=======
-
-    @Post('register')
-    async registerAccount(@Body() user:User):Promise<any>{
-        return await this.authService.registerUser(user);
-    }
-}
->>>>>>> 2e4f254ab883e7e5cda6614aacb61e53a25be9b5
