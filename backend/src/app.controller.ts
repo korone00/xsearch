ccc5e519570c7c2fd0ffad4ba9969262c0d323d8
korone <korone@gmail.com>
2023-07-20 16:33:27 +0900

    불필요한 getHello삭제

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,14 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOperation } from '@nestjs/swagger/dist';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @ApiOperation({summary:'hello world'})
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
