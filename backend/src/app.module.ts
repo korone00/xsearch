b594a9aaabdd6ffb257c8880da748703317a1255
korone <korone@gmail.com>
2023-07-20 16:33:55 +0900

    prettier 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,19 +10,19 @@ require('dotenv').config(); // .env variables load
 
 @Module({
   imports: [
-      UploadModule,
-      TypeOrmModule.forRoot({
-          type: 'postgres',
-          host: process.env.DB_HOST,
-          port: parseInt(process.env.DB_PORT),
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_DATABASE,
-          entities: [__dirname + '/**/*.entity.{ts,js}'],
-          synchronize: true, // shouldn't be used in production - otherwise you can lose production data.
-      }),
-      UserModule,
-      AuthModule,
+    UploadModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true, // shouldn't be used in production - otherwise you can lose production data.
+    }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
