8665e6d98b60b91e2deed0f4bacedd2d33c16068
shhyunn <limgm7@gmail.com>
2023-07-23 19:29:46 +0900

    minio function changes to class

--- engine/milvus/minio/minio_load.py
+++ engine/milvus/minio/minio_load.py
@@ -1,54 +1,145 @@
 import os
 from minio import Minio
 from minio.error import S3Error
+import requests
+from requests.exceptions import RequestException
+import zipfile
+from zipfile import BadZipFile
 
-# set client
-client = Minio(
-    "127.0.0.1:9000",
-    access_key="YVBEDpDv5r7mXK53zqOr",
-    secret_key="JAVY3ydROz0g4Y26jhAPW7EHfSuBu49JyCCL6zaG",
-    secure=False,
-)
-
-bucket_name = 'images' 
-
-try:
-    if not client.bucket_exists(bucket_name):
-        client.make_bucket(bucket_name)
+class minioLoader():
+    def __init__(self):
+        self.address = "127.0.0.1:9000"
+        self.access_key = "YVBEDpDv5r7mXK53zqOr"
+        self.secret_key ="JAVY3ydROz0g4Y26jhAPW7EHfSuBu49JyCCL6zaG"
+        self.client = None
+        self.bucket_name = None
+        self.scrip_dir = None
         
-except S3Error as err:
-    print(err)
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
-
-try:
-    folder_path = os.path.join(script_dir,"../reverse_image_search")
-    if not os.path.exists(folder_path):
-        raise Exception(f"Directory does not exist: {folder_path}.\n please run dataset.py in milvus")
+        
+    def set_minio(self):
+        self.client = Minio(
+            self.address,
+            access_key=self.access_key,
+            secret_key=self.secret_key,
+            secure=False,
+        )
     
-except Exception as e:
-    print(f'Error: {e}')
+    def set_bucket(self, bucket_name):
+        self.bucket_name = bucket_name
+        try:
+            if not self.client.bucket_exists(bucket_name):
+                self.client.make_bucket(bucket_name)
+                
+        except S3Error as err:
+            print(err)
+            
+    def set_ospath(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(script_dir)
+        self.script_dir = script_dir
+            
+    def set_path(self, download_link):
+        
+        self.set_ospath()
+        
+        # Specify the path where you want to save the downloaded file
+        save_path = os.path.join(self.script_dir,"reverse_image_search.zip")
+        self.save_path = save_path
+        
+        # Check if the file already exists before downloading
+        if not os.path.exists(save_path):
+            print("downloaded the image zip file.")
+            self.download_file(download_link, save_path)
+                
 
-#upload image files to minio server
-for subdir, dirs, files in os.walk(folder_path):
-    for file in files:
-        print(file)
-        if file.lower().endswith(".jpg") or file.lower().endswith(".png") or file.lower().endswith(".jpeg"):
-            file_path = os.path.join(subdir, file)
+    def download_file(self, url, save_path):
+        if os.path.exists(save_path):
+            print("File already exists. Skipping download.")
+            return
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # If the response was successful, no Exception will be raised
+            with open(save_path, "wb") as f:
+                f.write(response.content)
+                
+            print("File downloaded successfully.")
+            
+        except RequestException as e:
+            print(f"Failed to download the file. Error: {e}")
+            
+        return self.unzip_file(save_path)
 
-            s3_key = os.path.relpath(file_path, folder_path).replace("\\", "/")
 
-            try:
-                result = client.fput_object(
-                    bucket_name, s3_key, file_path,
-                )
-                print(
-                    "created {0} object; etag: {1}, version-id: {2}".format(
-                        result.object_name, result.etag, result.version_id,
-                    ),
-                )
-            except S3Error as err:
-                print(err)
-        else:
-            print("no such files.")
+    def unzip_file(self, zip_file_path):
+        try:
+            unzip_dir = os.path.splitext(zip_file_path)[0]
+            self.unzip_dir = unzip_dir
+            
+            if os.path.exists(unzip_dir):
+                print("Unzipped folder already exists. Skipping decompression.")
+                return
+
+            # Create the directory if it doesn't already exist
+            os.makedirs(unzip_dir, exist_ok=True)
+
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(unzip_dir)  # extract files into `unzip_dir`
+            print("File unzipped successfully.")
+            
+        except FileNotFoundError:
+            print(f"File {zip_file_path} does not exist.")
+            
+        except BadZipFile:
+            print(f"File {zip_file_path} is not a zip file or it is corrupted.")
+            
+        except PermissionError:
+            print(f"Permission denied when trying to create directory {unzip_dir}.")
+        
+        return 
+    
+    def load_minio(self):
+        try:
+            folder_path = os.path.join(self.script_dir, self.unzip_dir)
+            
+            if not os.path.exists(folder_path):
+                raise Exception(f"Directory does not exist: {folder_path}.\n please run dataset.py in milvus")
+            
+        except Exception as e:
+            print(f'Error: {e}')
+
+        #upload image files to minio server
+        for subdir, dirs, files in os.walk(folder_path):
+            for file in files:
+                print(file)
+                if file.lower().endswith(".jpg") or file.lower().endswith(".png") or file.lower().endswith(".jpeg"):
+                    file_path = os.path.join(subdir, file)
+
+                    s3_key = os.path.relpath(file_path, folder_path).replace("\\", "/")
+
+                    try:
+                        result = self.client.fput_object(
+                            self.bucket_name, s3_key, file_path,
+                        )
+                        
+                        print(
+                            "created {0} object; etag: {1}, version-id: {2}".format(
+                                result.object_name, result.etag, result.version_id,
+                            ),
+                        )
+                        
+                    except S3Error as err:
+                        print(err)
+                        
+                else:
+                    print("wrong format.")
+
+if __name__ == '__main__':
+    dataLoader = minioLoader()
+    dataLoader.set_minio()
+    dataLoader.set_bucket('images')
+    
+    download_link = "https://github.com/towhee-io/examples/releases/download/data/reverse_image_search.zip"
+    dataLoader.set_path(download_link)
+    
+    dataLoader.load_minio()
--- engine/milvus/minio/minio_test.py
+++ engine/milvus/minio/minio_test.py
@@ -2,36 +2,54 @@ import io
 import os
 from minio import Minio
 from minio.error import S3Error
+from minio_load import minioLoader
 
-# Create a client with the MinIO server, access key, and secret key
-client = Minio(
-    "127.0.0.1:9000",
-    access_key="YVBEDpDv5r7mXK53zqOr",
-    secret_key="JAVY3ydROz0g4Y26jhAPW7EHfSuBu49JyCCL6zaG",
-    secure=False,
-)
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
+class minioSearch(minioLoader):
+    def __init__(self):
+        super().__init__()
+        self.save_path = None
     
-img_path = 'train/yawl/n04612504_8870.JPEG'
-save_path = os.path.join(script_dir,'results', os.path.basename(img_path))
-
-try:
-    data = client.get_object("images", img_path)
-
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-    with open(save_path, "wb") as file:
-        file.write(data.read())
+    def set_imgpath(self, img_path):
+        if os.path.isabs(img_path):
+            img_path = self.change_path(img_path)
+            
+        self.set_ospath()
         
-except S3Error as err:
-    print(f"error: {err}")
+        save_path = os.path.join(self.script_dir,'results', os.path.basename(img_path))
+        self.save_path = save_path
+        
+    def get_image(self, img_path):
+        try:
+            data = self.client.get_object("images", img_path)
+
+            os.makedirs(os.path.dirname(self.save_path), exist_ok=True)
+
+            with open(self.save_path, "wb") as file:
+                file.write(data.read())
+                
+        except S3Error as err:
+            print(f"error: {err}")
     
-'''
+    def change_path(self, abs_path):
+        path_parts = abs_path.split('\\')  
+        new_path = os.path.join(*path_parts[-4:])
+        return new_path
+
+if __name__ == '__main__':
+    dataLoader = minioSearch()
+    dataLoader.set_minio()
+    dataLoader.set_bucket('images')
+    
+    img_path = 'train/yawl/n04612504_8870.JPEG'
+
+    dataLoader.set_imgpath(img_path)
+    dataLoader.get_image(img_path)
+    
+'''json examples
 {'img_path': 'reverse_image_search/test/apiary\\n02727426_4398.JPEG', 
 'pred': ['C:\\Users\\limgm\\xsearch\\engine\\milvus\\reverse_image_search\\train\\apiary\\n02727426_25725.JPEG', 
 'C:\\Users\\limgm\\xsearch\\engine\\milvus\\reverse_image_search\\train\\apiary\\n02727426_7944.JPEG', 
 'C:\\Users\\limgm\\xsearch\\engine\\milvus\\reverse_image_search\\train\\apiary\\n02727426_39745.JPEG'], 
 'score': ['24.710521697998047', '26.434852600097656', '43.78236770629883']}
 '''
+    
