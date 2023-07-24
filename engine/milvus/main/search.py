677e56db8d81a3631f3686b16b27e0e2cbe97864
shhyunn <limgm7@gmail.com>
2023-07-24 15:20:40 +0900

    connect engine and backend server

--- engine/milvus/main/search.py
+++ engine/milvus/main/search.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify
 from flask_restx import Resource, Namespace, fields
-from model import xsearch_engine
+from model import MilvusPredict
+from dataset import dataLoader
+from pymilvus import utility
 
 search = Namespace(name='search', description='Search similar image from milvus DB')
 
@@ -15,52 +17,38 @@ photo_model = search.model('Photo', {
 class SearchPhoto(Resource):
     @search.expect(photo_model)
     def post(self):
-        data = search.payload
+        #data = search.payload
+        data = request.get_json()  # Get the JSON data sent by the client
+        img_path = data['img_path']
         print(data)
         # Process the information received from NestJS /upload endpoint
         # For example, you can use the photo information to perform a search
         # and return the results as JSON
+        
         search_results = {
             'results': ['result1', 'result2', 'result3'],
             # Add more search results as needed
         }
+        
+        milvus = MilvusPredict()
+        milvus.connect()
+        collection_name = 'reverse_image_search'
+        
+        if utility.has_collection(collection_name):
+            # Get collection
+            milvus.setCollectionName(collection_name)
+            #
+            # #test
+            #img_path = 'C:\\Users\\limgm\\xsearch\\engine\\milvus\\main\\reverse_image_search\\train\\Afghan_hound\\n02088094_1045.JPEG' # 상대경로로는 안되는데 절대 경로로는 실행됨.
+            #jsons = milvus.search(img_path) #return json
+            #print(jsons)
+            
+        else:
+            dataLoader()
+            
+        # Do something with the image_path...
+        img_path_sample = 'reverse_image_search/test/apiary/*.JPEG'
+        result = milvus.search(img_path_sample)
 
         # Return the search results as JSON
-        return jsonify(search_results)
-
-
-
-# @search.route('', methods=['POST'])
-# class SearchSimilarImages(Resource):
-    
-#     # Receiving data from search using JSON in the request body
-#     def get():
-#         data = request.json
-#         key_value = data.get('key')
-#         return jsonify({'received_value': key_value})   
-    
-#     @search.response(400,'Please check file upload.')
-#     @search.response(200,'Success!')
-#     @search.response(500,'Error!')
-#     def post(self):
-#         """유사 이미지 검색"""
-#         try:
-#             # Extract the image file from the request
-#             if 'file' not in request.files:
-#                 return jsonify({'error': 'No file part in the request.'}), 400 #http status code
-
-#             file = request.files['file']
-#             if file.filename == '':
-#                 return jsonify({'error': 'No selected file.'}), 400 #http status code
-
-#             # Call the model.py function to perform the image search
-#             collection_name = 'reverse_image_search'  # Set the collection name as required
-#             #collection_name = request.files['category']
-            
-#             results = xsearch_engine(file, collection_name)
-
-#             return jsonify({'results': results}), 200 #http status code
-
-#         except Exception as e:
-#             return jsonify({'error': str(e)}), 500 #http status code
-        
+        return jsonify(result)
