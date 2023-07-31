a33f86bb885474060f1254eef094e10a05bbe46d
GaeunB <115343409+GaeunB@users.noreply.github.com>
2023-07-31 17:07:06 +0900

    Add ApiTags

--- backend/src/date/date.controller.ts
+++ backend/src/date/date.controller.ts
@@ -1,8 +1,25 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiBody, ApiConsumes,  ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('date')
+@ApiTags('Time API')
 export class DateController {
     @Get(':date')
+    @ApiOperation({summary:'Time API', description:'Timestamp'})
+    @ApiResponse({description:''})
+    @ApiConsumes('')
+    @ApiBody({
+      schema: {
+        type: 'object',
+        properties: {
+          file: { 
+            type: 'int',
+            format: 'binary'
+          },
+        },
+      },
+    })
+  
     getDate(@Param() params: { date: string }): any {
         // Do something with the name query parameter
         const date = params.date;
@@ -24,4 +41,7 @@ export class DateController {
         // console.log(Date.UTC(d))
         return { unix: d.getTime(), utc: d.toUTCString() };
     }
+     
+     
+    
 }
