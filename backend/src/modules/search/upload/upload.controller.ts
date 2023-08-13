1d53c33f5c4c24b5916f6c2ff328f9e843c12d0f
mhkim <rlafosem@gmail.com>
2023-08-13 15:52:07 +0000

    modify hard coding

--- backend/src/modules/search/upload/upload.controller.ts
+++ backend/src/modules/search/upload/upload.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from './config';
-import { ApiBody, ApiConsumes,  ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 
 
 @Controller('upload')
 @ApiTags('file upload API')
 export class uploadController {
+  constructor(private configService: ConfigService) {}
+
   @Post()
   @ApiOperation({summary:'파일 업로드 API', description:'파일을 저장 및 업로드힌다.'})
   @ApiResponse({description:'파일 업로드'})
@@ -36,7 +39,7 @@ export class uploadController {
     const loadFiles = new Promise((resolve, reject) => {
       fs.readdir('./uploads', (err: any, files: any[]) => {
         if (err) reject(err);
-
+        
         for (let i = 0; i < files.length; i++) {
           img_path = path.join('../../../../../uploads', `${files[i]}`);
         }
@@ -46,14 +49,16 @@ export class uploadController {
       });
     });
     await loadFiles;
-
+    
     console.log('Sending request to Flask server with data:', {
       collection_name: "reverse_image_search",
       img_path: img_path,
       category: ""
     });
-
-    const response = await axios.post('http://192.168.100.5:5000/search', {
+    
+    const serverAddress = this.configService.get<string>('SERVER_ADDRESS'); // .env 파일에서 SERVER_ADDRESS 변수 가져오기
+    
+    const response = await axios.post(`http://${serverAddress}:5000/search`, {
       filename: "",
       collection_name: "reverse_image_search",
       img_path: img_path,
@@ -65,3 +70,4 @@ export class uploadController {
     return responseData;
 }
 }
+
