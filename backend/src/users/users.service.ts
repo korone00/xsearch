04973c656ec9af2416ec89126ddbec60beb5af6a
jyuny <adela0603@kyonggi.ac.kr>
2023-08-17 00:30:26 +0900

    register guest id

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -12,19 +12,38 @@ export class UserService implements OnApplicationBootstrap {
     const hasAdmin = (users: User[]): boolean => {
       return users.some((user) => user.role === 'admin');
     };
+    const hasGuest = (users: User[]): boolean => {
+      return users.some((user) => user.id === 'guest');
+    };
     const isAdminPresent = hasAdmin(data);
+    const isGuestPresent = hasGuest(data);
     if (!isAdminPresent) {
       console.log('default admin 등록');
       //default admin 등록
       const defaultAdmin: Partial<User> = {
         id: 'admin',
-        password: '0000',
+        password: 'admin',
         name: '관리자',
         email: 'admin@gmail.com',
         phone: '010-0000-0000',
         role: 'admin',
       };
+      await this.save(defaultAdmin);
       console.log('default admin 등록 완');
+    }
+    if (!isGuestPresent) {
+      console.log('guest id 등록');
+      //default admin 등록
+      const defaultGuest: Partial<User> = {
+        id: 'guest',
+        password: 'guest',
+        name: 'guest',
+        email: 'guest@gmail.com',
+        phone: '010-0000-0000',
+        role: 'user',
+      };
+      await this.save(defaultGuest);
+      console.log('guest id 등록 완');
     }
   }
   async find(userId: string): Promise<User> {
