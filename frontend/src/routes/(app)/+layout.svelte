d192325fd0fe52d6c05484de0d2e7f35da777d48
Gaeun <bke2637@gmail.com>
2023-08-22 00:47:58 +0900

    add navbar_layout

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -2,6 +2,7 @@
 	export let data;
 	import { page } from '$app/stores';
 	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+
 	let activeUrl: string;
 	$: {
 		activeUrl = $page.url.pathname;
@@ -9,89 +10,82 @@
 	}
 </script>
 
+<nav class=" border-gray-200 bg-gray-50 border-b dark:border-gray-700">
+	<div class="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+		<a href="#" class="flex items-center">
+			<img src="xsearchlogo.png" class="h-8 mr-3" alt="Xsearch Logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+				>Xsearch</span
+			>
+		</a>
+		<button
+			data-collapse-toggle="navbar-solid-bg"
+			type="button"
+			class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			aria-controls="navbar-solid-bg"
+			aria-expanded="false"
+		>
+			<span class="sr-only">Open main menu</span>
+			<svg
+				class="w-5 h-5"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 17 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M1 1h15M1 7h15M1 13h15"
+				/>
+			</svg>
+		</button>
+		<div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+			<ul
+				class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+			>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+						aria-current="page">Home</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						>Search</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						>Statistics</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+						>Sign Out</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
 <section class="flex flex-1">
 	<div>
-		<Sidebar class="fixed h-full bg-gray-50">
-			<SidebarWrapper>
-				<a href="/" class="flex items-center pl-2.5 mb-5">
-					<img src="/xsearchlogo.png" class="h-6 mr-3 sm:h-7" alt="Flowbite Logo" />
-					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-						>Xsearch</span
-					>
-				</a>
-				<SidebarGroup>
-					<SidebarItem label="Home" href="/home" active={activeUrl === '/home'}>
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-					<SidebarItem label="Search" href="/search" active={activeUrl === '/search'}>
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-					<SidebarItem label="Statistics" href="/statistics" active={activeUrl === '/statistics'}>
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M1 1v14h16m0-9-3-2-3 5-3-2-3 4"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</SidebarGroup>
-				<SidebarGroup border>
-					<SidebarItem label="Profile" href="/profile" active={activeUrl === '/profile'}>
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-					{#if data.role === 'admin'}
-						<SidebarItem label="Userlist" href="/userlist" active={activeUrl === '/userlist'}>
+		<div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 border-r">
+			<Sidebar>
+				<SidebarWrapper>
+					<SidebarGroup>
+						<SidebarItem label="Home" href="/home" active={activeUrl === '/home'}>
 							<svelte:fragment slot="icon">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -101,34 +95,122 @@
 									stroke="currentColor"
 									class="w-6 h-6"
 									><path
-										d="M14 3a3 3 0 1 1-1.614 5.53M15 12a4 4 0 0 1 4 4v1h-3.348M10 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
 									/></svg
 								>
 							</svelte:fragment>
 						</SidebarItem>
-					{/if}
-					<SidebarItem label="Sign Out" href="/logout" active={activeUrl === '/'}>
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</SidebarGroup>
-			</SidebarWrapper>
-		</Sidebar>
+						<SidebarItem label="Search" href="/search" active={activeUrl === '/search'}>
+							<svelte:fragment slot="icon">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+									/></svg
+								>
+							</svelte:fragment>
+						</SidebarItem>
+						<SidebarItem label="Statistics" href="/statistics" active={activeUrl === '/statistics'}>
+							<svelte:fragment slot="icon">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M1 1v14h16m0-9-3-2-3 5-3-2-3 4"
+									/></svg
+								>
+							</svelte:fragment>
+						</SidebarItem>
+					</SidebarGroup>
+					<SidebarGroup border>
+						<SidebarItem label="Profile" href="/profile" active={activeUrl === '/profile'}>
+							<svelte:fragment slot="icon">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+									/></svg
+								>
+							</svelte:fragment>
+						</SidebarItem>
+						<SidebarItem label="Settings" href="/settings" active={activeUrl === '/settings'}>
+							<svelte:fragment slot="icon">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+									><path
+										d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z"
+									/>
+									<path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" /></svg
+								>
+							</svelte:fragment>
+						</SidebarItem>
+						{#if data.role === 'admin'}
+							<SidebarItem label="Userlist" href="/userlist" active={activeUrl === '/userlist'}>
+								<svelte:fragment slot="icon">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+										><path
+											d="M14 3a3 3 0 1 1-1.614 5.53M15 12a4 4 0 0 1 4 4v1h-3.348M10 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"
+										/></svg
+									>
+								</svelte:fragment>
+							</SidebarItem>
+						{/if}
+						<SidebarItem label="Sign Out" href="/logout" active={activeUrl === '/'}>
+							<svelte:fragment slot="icon">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"
+									/></svg
+								>
+							</svelte:fragment>
+						</SidebarItem>
+					</SidebarGroup>
+				</SidebarWrapper>
+			</Sidebar>
+		</div>
 	</div>
-	<div class="flex-1 justify-center ml-64 p-4">
+	<div class="flex-1 justify-center">
 		<slot />
 	</div>
 </section>
